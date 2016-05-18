a21b3a5e7ebdf56d2a5a2182d88abf8811e5c433
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-05-18 21:42:17 +0300

    TestClassroomAdded

---
+++ Assets/PrintRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class PrintRotation : MonoBehaviour
+    {
+        public GameObject text;
+        public GameObject text1;
+        public GameObject head;
+        
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            this.text.GetComponent<Text>().text = transform.rotation.eulerAngles.y.ToString();
+            this.text1.GetComponent<Text>().text = head.transform.rotation.eulerAngles.y.ToString();
+        }
+    }
+}
