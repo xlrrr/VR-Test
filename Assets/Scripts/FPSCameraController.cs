c6b119416c56bebac12c477f382d79ba4803ec88
xlrrr <staykov@mail.bg>
2016-06-09 23:16:27 +0300

    scene almost ready

---
+++ Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FPSCameraController : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject deathScreen;
+    public GameObject credits;
+    public GameObject creditsParticle;
+    public AudioClip deadSound;
+    public AudioSource mainMusic;
+
+    private bool IsDead;
+    // Use this for initialization
+    void Start()
+    {
+        IsDead = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Contains("Trap"))
+        {
+            Debug.Log("DEAD");
+            this.mainMusic.volume = 0.4f;
+            Invoke("PlayDeadSound", 1);
+            this.deathScreen.gameObject.SetActive(true);
+            this.TurnMovementOff();
+            switch (col.gameObject.tag)
+            {
+                case "SmashTrap":
+                    this.GetComponent<Animation>().Play("smashTrapDeath");
+                    break;
+                case "DoorTrap":
+                    this.GetComponent<Animation>().Play("doorTrapDeath");
+                    break;
+                case "PitTrap":
+                    this.GetComponent<Animation>().Play("pitTrapDeath");
+                    break;
+            }
+        }
+        if (col.gameObject.tag == "EndGame")
+        {
+            this.TurnMovementOff();
+            this.credits.SetActive(true);
+            this.creditsParticle.SetActive(true);
+        }
+    }
+   void TurnMovementOff()
+    {
+        this.GetComponent<FirstPersonController>().enabled = false;
+        this.GetComponent<Cardboard>().enabled = false;
+        this.head.GetComponent<CardboardHead>().enabled = false;
+    }
+
+    void PlayDeadSound()
+    {
+        if (!IsDead)
+        {
+            IsDead = true;
+            AudioSource.PlayClipAtPoint(deadSound, this.transform.position);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/ZombieFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieFollow : MonoBehaviour {
+    public GameObject target;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+    }
+}
